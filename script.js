51a067f84cbfe8f20c6766b652b465a34edf9d8e
Manuela Pacheco Malagón <m.pachecom2@uniandes.edu.co>
2023-06-19 14:23:51 -0500

    cant stop spinning

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
+const wheel = document.getElementById("wheel");
+const spinBtn = document.getElementById("spin-btn");
+const finalValue = document.getElementById("final-value");
+var rotationValues = [];
+var data = [];
+var pieColors = [];
+
+function setValues(){
+  let sum = 0;
+  let list = getList();
+  let colors = ["#8b35bc", "#b163da"];
+  degree = 360 / list.length;
+  for (let i = 0; i < list.length; i++) {
+    let value = list[i];
+    rotationValues.push({ minDegree: sum, maxDegree: sum + degree, value: value });
+    sum += degree;
+    pieColors.push(colors[i % colors.length]);
+    data.push(16);
+  }
+}
 
 function getList(){
   let j = document.getElementsByClassName("checked")
@@ -46,6 +66,8 @@ function showTask(){
 }
 
 function loadData() {
+  document.getElementById("spinner").style.display = "none";
+  
   let data = [
     "One Burrito",
     "Burger and chips",
@@ -73,6 +95,98 @@ function loadData() {
   }
 
 }
+
+function start(){
+  document.getElementById("selector").style.display = "none";
+  document.getElementById("spinner").style.display = "block";
+  setValues();
+  console.log(rotationValues);
+  console.log(pieColors);
+  console.log(data);
+  let myChart = new Chart(wheel, {
+    //Plugin for displaying text on pie chart
+    plugins: [ChartDataLabels],
+    //Chart Type Pie
+    type: "pie",
+    data: {
+      //Labels(values which are to be displayed on chart)
+      labels: getList(),
+      //Settings for dataset/pie
+      datasets: [
+        {
+          backgroundColor: pieColors,
+          data: data,
+        },
+      ],
+    },
+    options: {
+      //Responsive chart
+      responsive: true,
+      animation: { duration: 0 },
+      plugins: {
+        //hide tooltip and legend
+        tooltip: false,
+        legend: {
+          display: false,
+        },
+        //display labels inside pie chart
+        datalabels: {
+          color: "#ffffff",
+          formatter: (_, context) => context.chart.data.labels[context.dataIndex],
+          font: { size: 24 },
+        },
+      },
+    },
+  });
+  //display value based on the randomAngle
+  
+  //Spinner count
+  let count = 0;
+  //100 rotations for animation and last rotation for result
+  let resultValue = 51;
+  //Start spinning
+  spinBtn.addEventListener("click", () => {
+    spinBtn.disabled = true;
+    //Empty final value
+    finalValue.innerHTML = `<p>Good Luck!</p>`;
+    //Generate random degrees to stop at
+    let randomDegree = Math.floor(Math.random() * (355 - 0 + 1) + 0);
+    //Interval for rotation animation
+    let rotationInterval = window.setInterval(() => {
+      //Set rotation for piechart
+      /*
+      Initially to make the piechart rotate faster we set resultValue to 101 so it rotates 101 degrees at a time and this reduces by 1 with every count. Eventually on last rotation we rotate by 1 degree at a time.
+      */
+      myChart.options.rotation = myChart.options.rotation + resultValue;
+      //Update chart with new value;
+      myChart.update();
+      //If rotation>360 reset it back to 0
+      if (myChart.options.rotation >= 360) {
+        count += 1;
+        resultValue -= 5;
+        myChart.options.rotation = 0;
+      } else if (count > 15 && myChart.options.rotation == randomDegree) {
+        valueGenerator(randomDegree);
+        clearInterval(rotationInterval);
+        count = 0;
+        resultValue = 101;
+      }
+    }, 10);
+  });
+}
+
+function valueGenerator(angleValue){
+  console.log(angleValue);
+  for (let i of rotationValues) {
+    //if the angleValue is between min and max then display it
+    if (angleValue >= i.minDegree && angleValue <= i.maxDegree) {
+      finalValue.innerHTML = `<p>Value: ${i.value}</p>`;
+      spinBtn.disabled = false;
+      console.log(i.value);
+      break;
+    }
+  }
+};
 
 showTask();
 
